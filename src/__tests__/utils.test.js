cd4ac39645797649c6acc4191930b227f7bede39
commit-panda <sjmanyarkiy@gmail.com>
2025-12-02 16:19:15 +0300

    tests working

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,15 @@
 // Your tests here
+
+import { isPalindrome } from "../utils"
+
+
+describe('isPalindrome', () => {
+    it("returns true if palindrome and false if not", () => {
+        const word = "madam";
+
+        const palindrome = isPalindrome(word);
+
+        expect(palindrome).toBe(true);
+    })
+  
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,12 @@
 // Your code here
+
+export function isPalindrome(word){
+    let j = word.length - 1;
+    for(let i = 0; i < word.length / 2; i++){
+        if(word[i] != word[j]){
+            return false;
+        }
+        j--;
+    }
+    return true;
+}
